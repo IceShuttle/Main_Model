ddb647e8e0fc66b069c1df5e144730b87f6767e1
Shivang <shivangso23@iitk.ac.in>
2025-06-11 23:08:53 +0530

    updated imu

--- imu_utils.py
+++ imu_utils.py
@@ -7,6 +7,6 @@ geod = Geod(ellps="WGS84")
 
 def estimate_position(prev_lat, prev_lon, velocity, acceleration, heading, dt):
     # Compute displacement using kinematics: d = v * t + 0.5 * a * t²
-    displacement = (velocity * dt) + (0.5 * acceleration * dt**2)
+    displacement = (velocity * dt)
     new_lon,new_lat,_ = geod.fwd(prev_lon,prev_lat,np.degrees(heading),displacement)
     return new_lat, new_lon
